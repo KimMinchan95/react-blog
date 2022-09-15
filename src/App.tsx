96c330e2a663dd6fc581284b6b2a5300e9943154
KimMinchan95 <hialscks@gmail.com>
2022-09-15 23:35:40 +0900

    Konva Text

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 // Study List
 import StudyUseLayoutEffect from "./List/StudyUseLayoutEffect";
+import StudyKonva from "./List/StudyKonva";
 
 function App() {
   return (
     <div className="App">
-      <StudyUseLayoutEffect />
+      <StudyKonva />
     </div>
   );
 }
---
+++ src/List/StudyKonva.js
@@ -0,0 +1,11 @@
+import { Stage, Layer, Text } from "react-konva";
+
+const StudyKonva = () => {
+    return <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+            <Text text="let's practice Konva" fontSize={30} fontStyle={'bold'}/>
+        </Layer>
+    </Stage>
+}
+
+export default StudyKonva;
