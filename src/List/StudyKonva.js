11e3432425ad808812621d3571181cab6a4d0d85
KimMinchan95 <hialscks@gmail.com>
2022-09-17 00:31:36 +0900

    Add image

--- src/List/StudyKonva.js
+++ src/List/StudyKonva.js
@@ -1,10 +1,15 @@
-import { Stage, Layer, Text } from "react-konva";
+import { Stage, Layer, Text, Image } from "react-konva";
+import useImage from "use-image";
+
+import Jake from "../assets/img/jake.png";
 
 const StudyKonva = () => {
+  const [image] = useImage(Jake);
   return (
     <Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer>
         <Text text="let's practice Konva" fontSize={30} fontStyle={"bold"} />
+        <Image draggable x={50} y={50} image={image} />
       </Layer>
     </Stage>
   );
