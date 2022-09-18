999a8008a0ba12b53215a9250953f5cfcb8347a4
KimMinchan95 <hialscks@gmail.com>
2022-09-19 01:24:46 +0900

    Update image bound

--- src/List/StudyKonva.js
+++ src/List/StudyKonva.js
@@ -3,13 +3,44 @@ import useImage from "use-image";
 
 import Jake from "../assets/img/jake.png";
 
+const windowWith = window.innerWidth;
+const windowHeight = window.innerHeight;
+
+// jake size
+const jakeWidth = 200;
+const jakeHeight = 200;
+
+const calculatePosition = (position, imageSize, positionSize) => {
+  const maxPosition = positionSize - imageSize;
+  if (position < 0) {
+    return 0;
+  }
+  if (position > maxPosition) {
+    return maxPosition;
+  }
+  return position;
+};
+
 const StudyKonva = () => {
   const [image] = useImage(Jake);
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
+    <Stage width={windowWith} height={windowHeight}>
       <Layer>
         <Text text="let's practice Konva" fontSize={30} fontStyle={"bold"} />
-        <Image draggable x={50} y={50} image={image} />
+        <Image
+          draggable
+          x={50}
+          y={50}
+          image={image}
+          width={jakeWidth}
+          height={jakeHeight}
+          dragBoundFunc={({ x, y }) => {
+            return {
+              x: calculatePosition(x, jakeWidth, windowWith),
+              y: calculatePosition(y, jakeHeight, windowHeight),
+            };
+          }}
+        />
       </Layer>
     </Stage>
   );
