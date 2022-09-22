41b2d92293812b0fac4764ca9d4cffb598e0f895
KimMinchan95 <hialscks@gmail.com>
2022-09-23 05:28:32 +0900

    Start Study Cesium

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
 // Study List
 import StudyUseLayoutEffect from "./List/StudyUseLayoutEffect";
 import StudyKonva from "./List/StudyKonva";
+import StudyCesium from "./List/StudyCesium";
 
 function App() {
   return (
     <div className="App">
-      <StudyKonva />
+      <StudyCesium />
     </div>
   );
 }
---
+++ src/List/StudyCesium.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Cartesian3 } from "cesium";
+import { Viewer, Entity } from "resium";
+
+const StudyCesium = () => {
+  return <></>;
+};
+
+export default StudyCesium;
